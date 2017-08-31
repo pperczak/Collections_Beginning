37aec0cfc7b1a40d2a87e1a90487d46878594c6a
PLPK <ericl@o2.pl>
2017-08-31 18:33:37 +0200

    Initial commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("SilverScreen", 10,12);
+        theatre.getSeats();
+
+        if (theatre.reserveSeat("C03")) {
+            System.out.println("Please pay");
+        }else {
+            System.out.println("Sorry, seat is taken");
+        }
+
+
+        if (theatre.reserveSeat("C03")) {
+            System.out.println("Please pay");
+        }else {
+            System.out.println("Sorry, seat is taken");
+        }
+
+    }
+}
